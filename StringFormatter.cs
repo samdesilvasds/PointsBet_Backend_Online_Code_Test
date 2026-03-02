2425cd4f344c8a403c93c30540c31570fdcf653e
samdesilvasds <sam_de_silva@hotmail.com>
2026-03-02 12:09:57 +1100

    Refactored file
    Refactored file

---
+++ StringFormatter.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace PointsBet_Backend_Online_Code_Test
+{
+
+    /*
+    Improve a block of code as you see fit in C#.
+    You may make any improvements you see fit, for example:
+      - Cleaning up code
+      - Removing redundancy
+      - Refactoring / simplifying
+      - Fixing typos
+      - Any other light-weight optimisation
+    */
+    public class StringFormatter
+    {
+
+        public static string ToCommaSepatatedString(string[] items)
+        {
+            return string.Join(',', items);
+        }
+    }
+}
